be75aa83fc036b6c6f64aa66fbe238a2162f25be
Dakota Stephens <kota.helives@gmail.com>
2022-03-11 20:50:08 -0600

    Server configuration

---
+++ source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    string name;
+    cout << "Hello World" << endl << "Enter Name: ";
+    cin >> name;
+    cout << "Your Name is: " << name;
+}
