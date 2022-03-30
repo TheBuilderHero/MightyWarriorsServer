7aff1129a825d294feb1a92c5419f96cd94b15c2
Dakota Stephens <kota.helives@gmail.com>
2022-03-30 15:56:31 -0500

    -Kyle

--- Characters.h
+++ Characters.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cmath>    //Kyle
 
 #pragma once
 
@@ -15,5 +16,32 @@ class Characters {
             double weaponDamageLow; // the  lowest value of the weapons damage
             double weaponDamageHigh;// the highest value of the weapons damage
         };
+
+        void Human() {  //Kyle
+            name = username;
+            race = "Human";
+            baseHealth = 100;
+            baseArmor = 10;
+            baseMagicResistence = 5;
+            baseAbility = 1
+            baseAgility = 10
+            baseDamage = 5;
+            level = 0;
+        }
+
+        void increaseXP(){  //Kyle
+            int levels = 100;
+            int firstLevel = 500;
+            int lastLevel = 1000000;
+
+            double B = log((double)lastLevel / firstLevel) / (levels - 1);
+            double A = (double)firstLevel / (exp(B) - 1.0);
+
+            for (int i = 1; i <= levels; i++){
+                int oldXP = round(A * exp(B * (i - 1)));
+                int newXP = round(A * exp(B * i));
+                std::cout << i << " " << (newXP - oldXP) << std::endl;
+            }
+        }
 
 };
