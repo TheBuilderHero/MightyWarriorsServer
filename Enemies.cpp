6d00af5e50f366a7fb4dcb587d560e481f45a8f2
Quackattackjack <Quackattackjack@users.noreply.github.com>
2022-04-03 11:54:29 -0500

    Kyle Steed

--- Enemies.cpp
+++ Enemies.cpp
@@ -5,4 +5,12 @@ using namespace std;
 
 void Enemies::mutantElf() {
     cout << "Mutant Elf V2!" << endl;
+    name = "Mutant Elf";
+    health = 40;
+    attack = 10;
+    armor = 2;
+    magicAttack = 5;
+    magicResistence = 5;
+    level = 1;
+    xpDrop = 20;
 }
