6f0c2815a185d6cc1c0b9e3ed914bd3fde4487f9
Johnny-Blunder <Johnny-Blunder@users.noreply.github.com>
2022-04-20 06:38:05 -0500

    I like added some weapon stuff or something

--- PlayerElements/Weapons.cpp
+++ PlayerElements/Weapons.cpp
@@ -4,8 +4,61 @@
 
 using namespace std;
 
-Weapons::Weapons(/* args */) //Weapon class constructor
+Weapons::Weapons(int weaponChoice) 
 {
+    switch(weaponChoice){
+        case 1: 
+            weaponType = "Sword";
+            physicalDamage = 50;
+            magicDamage = 0;
+            psychicDamage = 0;
+            accuracy = 5; //maybe out of 10
+            break;
+        case 2: 
+            weaponType = "Bow";
+            physicalDamage = 30;
+            magicDamage = 0;
+            psychicDamage = 0;
+            accuracy = 4; 
+            break;
+        case 3: 
+            weaponType = "Dagger";
+            physicalDamage = 30;
+            magicDamage = 0;
+            psychicDamage = 0;
+            accuracy = 6; 
+            break;
+         case 4: 
+            weaponType = "Fire Rune";
+            physicalDamage = 10;
+            magicDamage = 45;
+            psychicDamage = 0;
+            accuracy = 8;
+            break;
+        case 5: 
+            weaponType = "Wind Rune";
+            physicalDamage = 15;
+            magicDamage = 35;
+            psychicDamage = 0;
+            accuracy = 9;
+            break;
+        case 6: 
+            weaponType = "Ice Spike";
+            physicalDamage = 40;
+            magicDamage = 90;
+            psychicDamage = 0;
+            accuracy = 2;
+            break;
+        case 7: 
+            weaponType = "Black Book";
+            physicalDamage = 20;
+            magicDamage = 20;
+            psychicDamage = 5;
+            accuracy = 10;
+            break;
+        default:
+        break;
+    }
 }
 
 Weapons::~Weapons() //Weapon class destructor
--- PlayerElements/Weapons.h
+++ PlayerElements/Weapons.h
@@ -1,12 +1,29 @@
 // added these files for the weapon class
+#include <string>
+
+using namespace std;
 
 class Weapons
 {
 private:
-    /* data */
+    string weaponType;
+    int physicalDamage, magicDamage, psychicDamage, accuracy;
+    int iron, wood, gems, feet, fruit, brains;
 public:
-    Weapons(/* args */);
+    Weapons(int weaponChoice);
     ~Weapons();
 
-}
+    int getPhysicalDamage(){ return physicalDamage + (iron * 2); }
+    int getMagicDamage(){ return magicDamage + (gems * 2); }
+    int getPsychicDamage(){ return psychicDamage + brains; }
+    int getAccuracy(){ return accuracy; }
+
+    int getIron(){ return iron; }
+    int getWood(){ return wood; }
+    int getGems(){ return gems; }
+    int getFeet(){ return feet; }
+    int getFruit(){ return fruit; }
+    int getBrains(){ return brains; }
+
+};
 
